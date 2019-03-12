29b191e74e5e378577758183ad53e7676043adbc
fabadea <fabadea@gmail.com>
2019-03-12 23:45:21 +0000

    daily kata

--- __tests__/simplifyDirections.test.js
+++ __tests__/simplifyDirections.test.js
@@ -1,7 +1,19 @@
 const { simplifyDirections } = require('../simplifyDirections')
 
 describe('simplifyDirections()', () => {
-  it('', () => {
-    // first test here
+  it('returns the same array if no directions to simplyfy', () => {
+    expect(simplifyDirections(['NORTH', 'EAST'])).toEqual(['NORTH', 'EAST'])
+  })
+  it('returns an empty array if the directions cancel one another out', () => {
+    expect(simplifyDirections(['NORTH', 'EAST'])).toEqual(['NORTH', 'EAST'])
+  })
+  it('returns an array with simplified directions', () => {
+    expect(simplifyDirections(['NORTH', 'SOUTH', 'WEST'])).toEqual(['WEST'])
+    expect(
+      simplifyDirections(['NORTH', 'WEST', 'SOUTH', 'WEST', 'EAST'])
+    ).toEqual(['WEST'])
+    expect(
+      simplifyDirections(['NORTH', 'EAST', 'SOUTH', 'WEST', 'NORTH', 'NORTH'])
+    ).toEqual(['NORTH', 'NORTH'])
   })
 })
--- simplifyDirections.js
+++ simplifyDirections.js
@@ -1,6 +1,21 @@
 // Please do not change the name of this function
-const simplifyDirections = (directions) => {
-  // Your code here
+const simplifyDirections = directions => {
+  const result = []
+  const refObj = {
+    NORTH: 'SOUTH',
+    SOUTH: 'NORTH',
+    WEST: 'EAST',
+    EAST: 'WEST'
+  }
+
+  for (let i = 0; i < directions.length; i++) {
+    if (directions.includes(refObj[directions[i]])) {
+      const index = directions.indexOf(refObj[directions[i]])
+      directions.splice(index, 1)
+    } else result.push(directions[i])
+  }
+
+  return result
 }
 
 module.exports = { simplifyDirections }
